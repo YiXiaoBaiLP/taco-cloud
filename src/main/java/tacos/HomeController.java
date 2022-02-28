de5bbfbf7d677d65c2fee9317b4a4517397e1cce
YiXiaoBai <yixiaobai.lp@qq.com>
2022-02-28 13:07:33 +0800

    Update "此类由,WebConfig.java替代"

--- src/main/java/tacos/HomeController.java
+++ src/main/java/tacos/HomeController.java
@@ -9,13 +9,15 @@ import org.springframework.web.bind.annotation.GetMapping;
  *
  */
 // @Controller：表示此类为Spring控制的Bean，控制器
-@Controller
-public class HomeController {
 
-//	// 处理对“/”路径的请求
-//	@GetMapping("/")
-//	public String home() {
-//		// 返回视图名称
-//		return "home";
-//	}
+//@Controller
+public class HomeController {
+/*
+	// 处理对“/”路径的请求
+	@GetMapping("/")
+	public String home() {
+		// 返回视图名称
+		return "home";
+	}
+*/
 }
